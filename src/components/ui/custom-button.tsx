12290684a567d47ad80b85b1edb5145af1878fd4
Artem <2929348+leipreachan@users.noreply.github.com>
2025-04-13 23:37:42 +0100

    button.tsx improvements

--- src/components/ui/custom-button.tsx
+++ src/components/ui/custom-button.tsx
@@ -1,10 +1,15 @@
 import { cn } from "@/lib/utils"
 import { Button } from "./button"
 
-const CustomButton = ({ className = "", ...props }) => {
+const CustomButton = ({ className = "", onClick, ...props }) => {
+    const onClickHandler = (e) => {
+        e.preventDefault();
+        return onClick(e);
+    }
     return (
         <Button
             className={cn("text-gray-400 hover:text-white bg-black hover:bg-gray-600", className)}
+            onClick={onClickHandler}
             {...props}
         />
     )
