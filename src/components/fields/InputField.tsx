8f89e657339eba132f377f8454e1634863176b44
Artem <2929348+leipreachan@users.noreply.github.com>
2025-04-13 23:50:26 +0100

    improved support of fields of type integer

--- src/components/fields/InputField.tsx
+++ src/components/fields/InputField.tsx
@@ -6,46 +6,58 @@ import Select from '../ui/custom-select';
 import DynamicFieldObject from './DynamicFieldObject';
 
 const InputField: React.FC<FormFieldProps> = ({ name, onChange, value, pattern, type, placeHolder = "" }) => {
-    if (type == "boolean") {
-        const allowedValues = ["false", "true"];
-        return (
-            <Select
-                name={name}
-                value={value === "" ? null : (value ? "true" : "false")}
-                onChange={(e) => onChange(e === undefined ? null : e === "true")}
-                propertyEnum={allowedValues}
-                multipleSelect={false}
-            />
-        );
-    } else if (type == "array") {
-        return (
-            <DynamicFieldArray name={name} value={value} onChange={onChange} />
-        )
-    } else if (type == "object") {
-        return (
-            <DynamicFieldObject name={name} value={value} onChange={onChange} />
-        )
-    } else {
-        let regexPattern = "";
-        if (pattern != undefined) {
-            try {
-                new RegExp(pattern as string);
-                regexPattern = pattern;
-            } catch(e) {
-                console.log(`String ${pattern} seems to be an invalid RegExp pattern`, e);
+    switch (type) {
+        case "boolean":
+            const allowedValues = ["false", "true"];
+            return (
+                <Select
+                    name={name}
+                    value={value === "" ? null : (value ? "true" : "false")}
+                    onChange={(e) => onChange(e === undefined ? null : e === "true")}
+                    propertyEnum={allowedValues}
+                    multipleSelect={false}
+                />
+            );
+        case "array":
+            return (
+                <DynamicFieldArray name={name} value={value} onChange={onChange} />
+            );
+        case "object":
+            return (
+                <DynamicFieldObject name={name} value={value} onChange={onChange} />
+            )
+        case "integer":
+            return (
+                <Input
+                    className={"bg-white" + (value ? "bg-amber-100" : "")}
+                    type={"text"}
+                    id={name}
+                    value={`${value}`}
+                    onChange={(e) => onChange(Number.parseInt(e.target.value))}
+                    placeholder={placeHolder}
+                />
+            )
+        default:
+            let regexPattern = "";
+            if (pattern != undefined) {
+                try {
+                    new RegExp(pattern as string);
+                    regexPattern = pattern;
+                } catch (e) {
+                    console.log(`String ${pattern} seems to be an invalid RegExp pattern`, e);
+                }
             }
-        }
-        return (
-            <Input
-                className={"bg-white" + (value ? "bg-amber-100" : "")}
-                type={"text"}
-                id={name}
-                value={`${value}`}
-                onChange={(e) => onChange(e.target.value)}
-                pattern={regexPattern}
-                placeholder={placeHolder}
-            />
-        )
+            return (
+                <Input
+                    className={"bg-white" + (value ? "bg-amber-100" : "")}
+                    type={"text"}
+                    id={name}
+                    value={`${value}`}
+                    onChange={(e) => onChange(e.target.value)}
+                    pattern={regexPattern}
+                    placeholder={placeHolder}
+                />
+            )
     }
 }
 
